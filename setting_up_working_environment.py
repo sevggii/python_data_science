d8f5e9b8750ee96d2e3d009c995fc6aa090eab43
sevgitr <sevgitr_@hotmail.com>
2023-08-03 00:40:48 +0300

    Virtual environment & package management

--- setting_up_working_environment.py
+++ setting_up_working_environment.py
@@ -1,1 +1,46 @@
 print("Hello AI Era")
+
+# Listing virtual environments:
+# conda env list
+
+# Creating a virtual environment:
+# conda create -n myenv
+
+# Activating virtual environment:
+# conda activate myenv / conda deactivate
+
+# List of installed packages:
+# conda list
+
+# Package installation:
+# conda install numpy
+
+# Package installation:
+# conda install numpy scipy pandas
+
+# Package deletion:
+# conda remove package_name
+
+# Installing packages based on a specific version:
+# conda install numpy=1.24
+
+# Package upgrade:
+# conda upgrade numpy
+
+# Upgrade of all packages:
+# conda upgrade -all
+
+# pip: pypi (pyton package index) package management tool
+# pip install pandas
+
+# Package installation by version:
+# pip install pandas==1.2.1
+
+# conda env export > environment.yaml
+# ls/dir : listing files
+
+
+
+
+
+
