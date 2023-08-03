e269432eec6a004a8e5401fd9ed215eb78bd5267
sevgitr <sevgitr_@hotmail.com>
2023-08-03 22:39:31 +0300

    added data analysis page

---
+++ data_analysis_with_python.py
@@ -0,0 +1,13 @@
+# - NumPy
+# - Pandas
+# - Data Visualization: Matplotlib & Seaborn
+# - Advanced Functional Exploratory Data Analysis
+
+##################
+#Reading Data
+##################
+
+import pandas as pd
+
+pd.read_csv()
+
